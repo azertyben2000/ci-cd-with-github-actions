1f8e05f476551d1e9832ada8bd22e576e5885487
benjamincrt <azertyben2000@orange.fr>
2024-01-09 12:49:56 +0100

    add routes /get for test

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ items = []
 def index():
     return render_template('index.html', items=items)
 
+@app.route('/get')
+def get():
+    return items
+
 @app.route('/add', methods=['POST'])
 def add_item():
     item = request.form.get('item')
